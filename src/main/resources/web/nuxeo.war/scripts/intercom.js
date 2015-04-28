3c0ec776dc84d03dd12ea94c1d09eb56a311692d
Angajime <tarazaky@alu.uma.es>
2015-04-28 09:50:08 +0200

    Using localStorage now.

--- src/main/resources/web/nuxeo.war/scripts/intercom.js
+++ src/main/resources/web/nuxeo.war/scripts/intercom.js
@@ -6,28 +6,34 @@ window.intercomSettings = {
     app_id: "sfpqql0z"
   };
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/sfpqql0z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-Intercom('trackEvent', 'Navigation');
 //----------------------------
+
+//Check localStorage for instructions
+var transition = localStorage.getItem('athento-intercom-transition');
+if(transition !== null){
+	Intercom('trackEvent', transition);
+	localStorage.removeItem('athento-intercom-transition');
+}
 
 // Create Document
 jQuery('#document_create\\:nxw_documentCreateButtons_CREATE_DOCUMENT').click(function(){
 	var pathname = window.location.pathname;
 	if(pathname.indexOf("UserWorkspaces") > -1){
-		Intercom('trackEvent', 'new document on UserWorkspace');
+		localStorage.setItem('athento-intercom-transition', 'new document on UserWorkspace');
 	}else {
 		var type = jQuery(".mainNoTabs h3").html().replace("Create a new document ","").replace("\n","").trim();
-		Intercom('trackEvent', 'new document: '+type);
+		localStorage.setItem('athento-intercom-transition', 'new document: '+type);
 	}
 });
 
 // Create new Navigation Tree
 jQuery('#document_create\\:nxw_documentCreateButtons_CREATE_WORKSPACE').click(function(){
-	Intercom('trackEvent', 'new workspace');
+	localStorage.setItem('athento-intercom-transition', 'new workspace');
 });
 
 // Create new User
 var fn_user = function(){
-	Intercom('trackEvent', 'new user');
+	localStorage.setItem('athento-intercom-transition', 'new user');
 }
 // Event binding on dynamically created elements
 jQuery('body').on('click', '#createUserView\\:createUser\\:button_save_and_invite', fn_user);
@@ -44,12 +50,12 @@ jQuery('.nuxeoDrivePackages a').click(function(){
 
 // Workflow start
 jQuery('#nxl_grid_summary_layout\\:nxw_summary_document_route_form\\:nxw_start_route_widget_start_route').click(function(){
-	Intercom('trackEvent', 'workflow start');
+	localStorage.setItem('athento-intercom-transition', 'workflow start');
 });
 
 // Drag 'n' Drop
 jQuery('body').on("drop",function(event) { 
-	Intercom('trackEvent', 'drag and drop');
+	localStorage.setItem('athento-intercom-transition', 'drag and drop');
 });
 
 
