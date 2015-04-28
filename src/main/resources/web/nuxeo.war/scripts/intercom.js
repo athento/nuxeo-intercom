9e2505f80810f3c3825d411f77493e2d448f8530
Angajime <tarazaky@alu.uma.es>
2015-04-28 09:24:37 +0200

    Commit before complete modification.

--- src/main/resources/web/nuxeo.war/scripts/intercom.js
+++ src/main/resources/web/nuxeo.war/scripts/intercom.js
@@ -6,6 +6,7 @@ window.intercomSettings = {
     app_id: "sfpqql0z"
   };
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/sfpqql0z';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+Intercom('trackEvent', 'Navigation');
 //----------------------------
 
 // Create Document
@@ -48,7 +49,7 @@ jQuery('#nxl_grid_summary_layout\\:nxw_summary_document_route_form\\:nxw_start_r
 
 // Drag 'n' Drop
 jQuery('body').on("drop",function(event) { 
-	alert("Dropped!"); 
+	Intercom('trackEvent', 'drag and drop');
 });
 
 
